bef5a5c74001aabfec5823cf174e7cf3fd1837b8
Paskanas <kaspe1992@gmail.com>
2022-05-18 12:31:15 +0300

    fixed carousel img in center

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,41 +1,74 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Carousel, Image } from 'react-bootstrap'
-import Loader from './Loader'
-import Message from './Message'
-import { useDispatch, useSelector } from 'react-redux'
-import { listProducts } from '../actions/productActions'
+import React, {
+  useEffect,
+} from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Carousel,
+  Image,
+} from 'react-bootstrap';
+import Loader from './Loader';
+import Message from './Message';
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 const ProductCaousel = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const productList = useSelector((state) => state.productList)
-  const { loading, error, products, page, pages } = productList
+  const productList = useSelector(
+    (state) => state.productList
+  );
+  const {
+    loading,
+    error,
+    products,
+    page,
+    pages,
+  } = productList;
 
   useEffect(() => {
-    dispatch(listProducts('', '', true))
-  }, [dispatch])
+    dispatch(
+      listProducts('', '', true)
+    );
+  }, [dispatch]);
 
   return loading ? (
     <Loader />
   ) : error ? (
-    <Message variant='danger'>{error}</Message>
+    <Message variant="danger">
+      {error}
+    </Message>
   ) : (
-    <Carousel pause='hover' className='bg-dark'>
+    <Carousel
+      pause="hover"
+      className="bg-dark"
+    >
       {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className='carousel-caption'>
+        <Carousel.Item
+          key={product._id}
+          className="text-center"
+        >
+          <Link
+            to={`/product/${product._id}`}
+          >
+            <Image
+              src={product.image}
+              alt={product.name}
+              fluid
+            />
+            <Carousel.Caption className="carousel-caption">
               <h2>
-                {product.name} (${product.price})
+                {product.name} ($
+                {product.price})
               </h2>
             </Carousel.Caption>
           </Link>
         </Carousel.Item>
       ))}
     </Carousel>
-  )
-}
+  );
+};
 
-export default ProductCaousel
+export default ProductCaousel;
